ea71ab1e2556e8d43d1ee1dba5c5071bdf92763a
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 13:36:18 +0300

    While Loop
    internship

---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loop
+            int number = 100;
+            while (number >= 0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
